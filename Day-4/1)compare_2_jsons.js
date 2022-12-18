7c7086a6abe77dc14ce6f7699bf40f9222b71f7a
Neslyn Chellaiah <neslynchellaiah@gmail.com>
2022-12-18 12:54:15 +0530

    Update 1)compare_2_jsons.js

--- Day-4/1)compare_2_jsons.js
+++ Day-4/1)compare_2_jsons.js
@@ -2,7 +2,7 @@ var obj1 = {name: "Person 1", age: 5}
 var obj2 = {age: 5, name: "Person 1"}
 
 let compareObjects = () => {
-    for (key in obj1) {
+    for (var key in obj1) {
         if(obj1[key] !== obj2[key]) {
             return false;
         }
